7aaa606b9daf322ed290cc009674bb32be2e9007
HastagKc <chaudharykshittiz950@gmail.com>
2023-03-03 12:40:25 +0545

    C++

---
+++ basic Programming/datatypes.cpp
@@ -0,0 +1,15 @@
+//? datatype in c Programming
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    //    string name;
+    cout << "Enter any name ";
+    cin >> name;
+
+    cout << "Hello " << name;
+    return 0;
+}
