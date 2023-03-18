e2efd798f8c2615c9262bd3979d421d20f3cd3c9
HastagKc <chaudharykshittiz950@gmail.com>
2023-03-18 14:04:28 +0545

    array

---
+++ sorting algo/bubbleSort1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+// creating a bubble sort function
+void bubbleSort(int arr[])
+{
+    // int length = arr.size();
+}
+int main()
+{
+    // learning bubble sort algo
+
+    // in bubble sort two adjacent element
+    // where compared and swap if first
+    // one is greater than second one
+
+    // calling bubbleSort function
+
+    int arr[5] = {10, 20, 30, 40, 50};
+    bubbleSort(arr);
+
+    return 0;
+}
