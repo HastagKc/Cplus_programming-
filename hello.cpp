9771b9ba2ac1217f97c2e127f17a3bb569a251ee
HastagKc <chaudharykshittiz950@gmail.com>
2023-03-27 21:22:55 +0545

    2d array

--- hello.cpp
+++ hello.cpp
@@ -1,20 +1,8 @@
-// Program to print positive number entered by the user
-// If the user enters a negative number, it is skipped
-
 #include <iostream>
 using namespace std;
 
 int main()
 {
-  int number;
-  cout << "Enter an integer: ";
-  cin >> number;
-
-  // checks if the number is positive
-  if (number > 0)
-  {
-    cout << "You entered a positive integer: " << number << endl;
-  }
-  cout << "This statement is always executed.";
-  return 0;
+    cout << "Hello World" << endl;
+    return 0;
 }
